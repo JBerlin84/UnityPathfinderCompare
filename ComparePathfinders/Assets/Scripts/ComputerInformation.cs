803601e14a006bc5c0d05e939b165a42d823d1f5
Jimmy Berlin <berlin.jimmy@gmail.com>
2017-11-02 16:02:39 +0100

    Adding script that reads computer information

---
+++ ComparePathfinders/Assets/Scripts/ComputerInformation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class ComputerInformation : MonoBehaviour {
+
+	string deviceType;
+	string operatingSystem;
+	string logicalCores;
+	string processorType;
+	string processorFrequency;
+	string systemMemorySize;
+	string graphicsDeviceVendor;
+	string graphicsDeviceName;
+	string graphicsDeviceType;
+	string graphicsMemorySize;
+
+	void Start () {
+		deviceType = SystemInfo.deviceType.ToString();
+		operatingSystem = SystemInfo.operatingSystem.ToString();
+		logicalCores = SystemInfo.processorCount.ToString();
+		processorType = SystemInfo.processorType.ToString();
+		processorFrequency = SystemInfo.processorFrequency.ToString();
+		systemMemorySize = SystemInfo.systemMemorySize.ToString();
+		graphicsDeviceVendor = SystemInfo.graphicsDeviceVendor.ToString();
+		graphicsDeviceName = SystemInfo.graphicsDeviceName.ToString();
+		graphicsDeviceType = SystemInfo.graphicsDeviceType.ToString();
+		graphicsMemorySize = SystemInfo.graphicsMemorySize.ToString();
+
+		print(ToString());
+	}
+
+	public override string ToString() {
+		string reply = 	"Device type: " + deviceType + "\n" +
+						"Operating system: " + operatingSystem + "\n" +
+						"Processor type: " + processorType + "\n" +
+						"Logical cores: " + logicalCores + "\n" +
+						"Processor frequency: " + processorFrequency + "\n" +
+						"System memory size: " + systemMemorySize + "\n" +
+						"Graphics device vendor: " + graphicsDeviceVendor + "\n" +
+						"Graphics device name: " + graphicsDeviceName + "\n" +
+						"Graphics device type: " + graphicsDeviceType + "\n" +
+						"Graphics memory size: " + graphicsMemorySize;
+
+		return reply;
+	}
+}
---
+++ ComparePathfinders/Assets/Scripts/PrintInformation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintInformation : MonoBehaviour {
+
+	ComputerInformation info = new ComputerInformation();
+
+	// Use this for initialization
+	void Start () {
+		print(info.ToString());
+	}
+}
