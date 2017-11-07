d69048aef06eaccde560c480a54d40a50b02d548
Jimmy Berlin <berlin.jimmy@gmail.com>
2017-11-07 20:42:56 +0100

    we are now clocking, but for some reason this nav mesh seems incredibly fast.

--- ComparePathfinders/Assets/Scripts/NavSimulation.cs
+++ ComparePathfinders/Assets/Scripts/NavSimulation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Diagnostics;
 
 public class NavSimulation : MonoBehaviour {
 
@@ -9,20 +10,56 @@ public class NavSimulation : MonoBehaviour {
 	public Transform target;
 	public WorldGenerator worldGenerator;
 
+	public int seed = 0;
+
+	float[,] world;
+
+	public int numberOfSimulations;
+	private Vector3[] startPositions;
+	private Vector3[] targetPositions;
 	// Use this for initialization
+
 	void Start () {
-		float[,] world = worldGenerator.World;
 
-		// Position agent and target
-		int xSize = world.GetLength(0);
-		int zSize = world.GetLength(1);
-		print("xSize: " + xSize);
-		print("zSize: " + zSize);
+		Random.InitState(seed);
+		world = worldGenerator.World;
 
-		agent.transform.position = new Vector3(1, world[1,1], 1);
-		target.transform.position = new Vector3(xSize-2, world[xSize-2, zSize-2], zSize-2);
+		prepareSimulations(world.GetLength(0), world.GetLength(1));
+		runSimulations();
+		
+	}
 
-		NavMeshPath path = new NavMeshPath();
-		agent.CalculatePath(target.position, path);
+	void runSimulations() {
+		Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
+
+		for(int i=0;i<numberOfSimulations;i++) {
+			agent.transform.position = startPositions[i];
+			target.transform.position = targetPositions[i];
+			NavMeshPath path = new NavMeshPath();
+			if(agent.CalculatePath(target.position, path)) {
+				// print("Path calculated between " + startPositions[i].ToString() + " and " + targetPositions[i].ToString() + "!");
+			} else {
+				// print("Path could not be found between " + startPositions[i].ToString() + " and " + targetPositions[i].ToString() + "!");
+			}
+		}
+		watch.Stop();
+		print("The simulation took: " + watch.ElapsedMilliseconds + " milliseconds");
+	}
+
+	void prepareSimulations(int xSize, int zSize) {
+		startPositions = new Vector3[numberOfSimulations];
+		targetPositions = new Vector3[numberOfSimulations];
+
+		for(int i=0;i<numberOfSimulations;i++) {
+			// Start
+			int x = Random.Range(0, xSize-1);
+			int z = Random.Range(0, zSize-1);
+			startPositions[i] = new Vector3(x, world[x,z], z);
+
+			// Target
+			x = Random.Range(0, xSize-1);
+			z = Random.Range(0, zSize-1);
+			targetPositions[i] = new Vector3(x, world[x,z], z);
+		}
 	}
 }
--- ComparePathfinders/Assets/Scripts/WorldGenerator.cs
+++ ComparePathfinders/Assets/Scripts/WorldGenerator.cs
@@ -47,7 +47,6 @@ public class WorldGenerator : MonoBehaviour {
 		GenerateMesh();
 		meshFilter.mesh = mesh;
 
-		//transform.localScale = new Vector3(worldWidth, worldHeight, worldDepth);
 		navMeshSurface.BuildNavMesh();
 	}
 
