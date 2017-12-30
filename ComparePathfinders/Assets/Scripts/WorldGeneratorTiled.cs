efb51f11233eef246f10daaae522acd1bc25fe15
Jimmy Berlin <berlin.jimmy@gmail.com>
2017-12-30 03:01:13 +0100

    Now have connected map after generation

--- ComparePathfinders/Assets/Scripts/WorldGeneratorTiled.cs
+++ ComparePathfinders/Assets/Scripts/WorldGeneratorTiled.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class WorldGeneratorTiled : MonoBehaviour {
 
+	public string seed;
+	public bool randomize;
+
+	[Range(0,1)]
 	public float fillPercentage;
 	public int worldWidth;
 	public int worldHeight;
@@ -12,19 +16,45 @@ public class WorldGeneratorTiled : MonoBehaviour {
 	public Transform navigationFloor;
 	public Transform obstacle;
 
-	private int[,] world;	// 0 is wall, 1 is passable.
+	private int[,] world;	// 1 is wall, 0 is passable.
 
 	void Awake() {
+		if(randomize) {
+			Random.InitState((int)System.DateTime.Now.Ticks);
+		} else {
+			Random.InitState(seed.GetHashCode());
+		}
+
 		world = new int[worldWidth, worldHeight];
 
+		for(int x = 0;x<worldWidth;x++) {
+			for(int y=0;y<worldHeight;y++) {
+				world[x,y] = 0;
+			}
+		}
+
+		int obstacleCount = (int)(worldWidth*worldHeight*fillPercentage);
+		for(int i=0;i<obstacleCount;i++) {
+			int xPos = Random.Range(0,worldWidth);
+			int yPos = Random.Range(0,worldHeight);
+
+			// If position is not taken.
+			if(world[xPos,yPos] == 0) {
+				if(CheckConnectivity(xPos, yPos)) {
+					world[xPos, yPos] = 1;
+				}
+			}
+		}
+
+/*
 		for(int i=0;i<worldWidth;i++) {
 			for(int j=0;j<worldHeight;j++) {
 				world[i,j] = Random.Range(0f,1f) < fillPercentage ? 0 : 1;
 			}
 		}
-
-		navigationFloor.localScale = new Vector3(worldHeight, worldWidth, 0);
-		navigationFloor.position = new Vector3(worldWidth/2 - 0.5f, 0, worldHeight/2 - 0.5f);
+*/
+//		navigationFloor.localScale = new Vector3(worldHeight, worldWidth, 0);
+//		navigationFloor.position = new Vector3(worldWidth/2 - 0.5f, 0, worldHeight/2 - 0.5f);
 	}
 
 	// Use this for initialization
@@ -42,5 +72,68 @@ public class WorldGeneratorTiled : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	bool CheckConnectivity(int xPos, int yPos) {
+		bool existFree = false;
+		Vector2 firstFree = new Vector2(-1f, -1f);
+		//find first free slot.
+		for(int x=0;x<worldWidth;x++) {
+			for(int y=0;y<worldHeight;y++) {
+				if(world[x,y] == 0) {
+					firstFree = new Vector2(x,y);
+					existFree = true;
+					break;
+				}
+			}
+			if(existFree) {			// Compiler sais that this is unreachable, which it is not.
+				break;
+			}
+		}
+
+		if(!existFree) {
+			return false;
+		}
+
+		int currentFreeCount = 0;
+		for(int x=0;x<worldWidth;x++) {
+			for(int y=0;y<worldHeight;y++) {
+				if(world[x,y] == 0) {
+					++currentFreeCount;
+				}
+			}
+		}
+
+		bool[,] mapFlags = new bool[worldWidth, worldHeight];	// Helper variable to theck all the available tiles.
+		Queue<Vector2> queue = new Queue<Vector2>();
+		queue.Enqueue(firstFree);
+
+		int accessibleTileCount = 1;
+		mapFlags[(int)firstFree.x, (int)firstFree.y] = true;
+		mapFlags[xPos, yPos] = true;
+
+		while(queue.Count > 0) {
+			Vector2 tile = queue.Dequeue();
+
+			for(int x=-1;x<=1;x++) {
+				for(int y=-1;y<=1;y++) {
+					int neighbourX = (int)tile.x+x;
+					int neighbourY = (int)tile.y+y;
+
+					// Only check nondiagonal ones.
+					if(x==0 || y==0) {
+						if (neighbourX >= 0 && neighbourX < worldWidth && neighbourY >= 0 && neighbourY < worldHeight) {
+							if (!mapFlags [neighbourX, neighbourY] && world[neighbourX, neighbourY] == 0) {
+								mapFlags [neighbourX, neighbourY] = true;
+								queue.Enqueue (new Vector2 (neighbourX, neighbourY));
+								accessibleTileCount++;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return accessibleTileCount == currentFreeCount - 1;
 	}
 }
