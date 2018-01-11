56a2bcab9cdfdbe108ac981fc0fe400475c08394
Jimmy Berlin <berlin.jimmy@gmail.com>
2018-01-11 15:19:14 +0100

    optimized to approximately 60fps on idle program run now

--- ComparePathfinders/Assets/Scripts/Stresser.cs
+++ ComparePathfinders/Assets/Scripts/Stresser.cs
@@ -7,27 +7,22 @@ public class Stresser : MonoBehaviour {
 	private Vector3 position;
 
 	public GameObject box;
-
 	public int boxCount;
-	private int currentBoxCount;
+	public float boxScale;
 
 	// Use this for initialization
 	void Start () {
 		position = transform.position;
 		for(int i=0;i<boxCount;i++) {
 			Vector3 spawnPos = new Vector3(position.x+Random.Range(-4,4), position.y+Random.Range(-4,4), position.z+Random.Range(-4,4));
-			Instantiate(box, spawnPos, Quaternion.identity);
+			GameObject newBox = Instantiate(box, spawnPos, Quaternion.identity);
+			newBox.transform.localScale = new Vector3(boxScale, boxScale, boxScale);
+
 		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		/*
-		if(currentBoxCount < boxCount) {
-			Instantiate(box, transform.position, Quaternion.identity);
-			currentBoxCount++;
-		}*/
-
+	void FixedUpdate () {
 		transform.Rotate(0,0,0.5f);
 	}
 }
