789e7493c60ae17543c1951f9c954daaabc14734
Jimmy Berlin <berlin.jimmy@gmail.com>
2018-01-11 19:41:06 +0100

    Removed all the add in navigation mesh things

--- ComparePathfinders/Assets/Scripts/NavSimulation.cs
+++ ComparePathfinders/Assets/Scripts/NavSimulation.cs
@@ -8,7 +8,6 @@ public class NavSimulation : MonoBehaviour {
 
 	public NavMeshAgent agent;
 	public Transform target;
-	public WorldGenerator worldGenerator;
 	public WorldGeneratorTiled worldGeneratorTiled;
 
 	public int seed = 0;
@@ -50,17 +49,6 @@ public class NavSimulation : MonoBehaviour {
 			print("The built in simulation took: " + builtInSimulationTimer.ElapsedTicks + " ticks. (" + builtInSimulationTimer.ElapsedMilliseconds + " ms)\n" + 
 					"The A* single threaded simulation took: " + aStarSingleThreadedSimulationTimer.ElapsedTicks + " ticks. (" + aStarSingleThreadedSimulationTimer.ElapsedMilliseconds + " ms)");
 		}
-
-		/*if(!builtInSimulatorFinished) {
-			runBuiltInSimulation();
-		} else if (!aStarSingleThreadedFinished) {
-			runAStarSingleThreadedSimulation();
-		}
-		
-		if(builtInSimulatorFinished && aStarSingleThreadedFinished) {
-			print("The built in simulation took: " + builtInSimulationTimer.ElapsedTicks + " ticks. (" + builtInSimulationTimer.ElapsedMilliseconds + " ms)\n" + 
-					"The A* single threaded simulation took: " + aStarSingleThreadedSimulationTimer.ElapsedTicks + " ticks. (" + aStarSingleThreadedSimulationTimer.ElapsedMilliseconds + " ms)");
-		}*/
 	}
 
 	// This seems to work
@@ -116,59 +104,6 @@ public class NavSimulation : MonoBehaviour {
 		}
 	}
 
-
-
-/*
-	void runAStarSingleThreadedSimulation() {
-		Stopwatch frameWatch = System.Diagnostics.Stopwatch.StartNew();
-
-		if(simulationsRunSoFar < numberOfSimulations) {
-			while(simulationsRunSoFar < numberOfSimulations && frameWatch.ElapsedMilliseconds < 1) {
-				agent.transform.position = startPositions[simulationsRunSoFar];
-				target.transform.position = targetPositions[simulationsRunSoFar];
-
-				aStarSingleThreadedSimulationTimer.Start();
-				aStar.Setup(agent.transform.position, target.transform.position);
-				if(aStar.CalculatePath()) {
-				// print("WE FOUND SOMETHING WITH A*");
-				} else {
-				//	print("we did not found anything with A* :'(");
-				}
-				aStarSingleThreadedSimulationTimer.Stop();
-
-				++simulationsRunSoFar;
-			}
-		} else {
-			aStarSingleThreadedFinished = true;
-			simulationsRunSoFar = 0;
-		}
-	}
-
-	void runBuiltInSimulation() {
-		Stopwatch frameWatch = System.Diagnostics.Stopwatch.StartNew();
-		
-		if(simulationsRunSoFar < numberOfSimulations) {
-			while(simulationsRunSoFar < numberOfSimulations && frameWatch.ElapsedMilliseconds < 1) {
-				agent.transform.position = startPositions[simulationsRunSoFar];
-				target.transform.position = targetPositions[simulationsRunSoFar];
-				
-				builtInSimulationTimer.Start();
-				NavMeshPath path = new NavMeshPath();
-				if(agent.CalculatePath(target.position, path)) {
-					//print("Path calculated between " + startPositions[simulationsRunSoFar].ToString() + " and " + targetPositions[simulationsRunSoFar].ToString() + "!");
-				} else {
-					//print("Path could not be found between " + startPositions[simulationsRunSoFar].ToString() + " and " + targetPositions[simulationsRunSoFar].ToString() + "!");
-				}
-				builtInSimulationTimer.Stop();
-				
-				++simulationsRunSoFar;
-			}
-		} else {
-			builtInSimulatorFinished = true;
-			simulationsRunSoFar = 0;
-		}
-	}
-*/
 	void prepareSimulations(int xSize, int zSize) {
 		startPositions = new Vector3[numberOfSimulations];
 		targetPositions = new Vector3[numberOfSimulations];
