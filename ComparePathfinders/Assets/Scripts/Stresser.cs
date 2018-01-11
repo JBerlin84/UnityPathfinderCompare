52d31a4c95100dc75dc1f491af6a222a579ae3b7
Jimmy Berlin <berlin.jimmy@gmail.com>
2018-01-11 15:10:15 +0100

    Adding some things to force the computer to work

---
+++ ComparePathfinders/Assets/Scripts/Stresser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stresser : MonoBehaviour {
+
+	private Vector3 position;
+
+	public GameObject box;
+
+	public int boxCount;
+	private int currentBoxCount;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+		for(int i=0;i<boxCount;i++) {
+			Vector3 spawnPos = new Vector3(position.x+Random.Range(-4,4), position.y+Random.Range(-4,4), position.z+Random.Range(-4,4));
+			Instantiate(box, spawnPos, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if(currentBoxCount < boxCount) {
+			Instantiate(box, transform.position, Quaternion.identity);
+			currentBoxCount++;
+		}*/
+
+		transform.Rotate(0,0,0.5f);
+	}
+}
